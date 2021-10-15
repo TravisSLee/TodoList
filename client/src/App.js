4bb84ef7cb6bc40bd2784ead66ebcff940089145
TravisSLee <travisslee223@gmail.com>
2021-10-15 14:51:35 -0400

    update dependencies and components

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import TodoList from './TodoList'
+import uuidv4 from './uuidv4'
 
 function App() {
+  
+  const [todos, setTodos ] = useState([])
+  const todoNameRef = useRef
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, ]
+    })
+    todoNameRef.current.value = null
+  }
+  
   return (
     <>
-      <TodoList />
-      <input type="text" />
-      <button type="button">Add Todo</button>
-      <button type="button">Clear Completed Todo</button>
-      <div>Number of Todos left</div> 
+      <TodoList todos={todos}/>
+      <input ref={todoNameRef} type="text" />
+      <button type="button" onClick={handleAddTodo}>Add Todo</button>
+      <button type="button">Clear Todos</button>
+      <div>{todos.length} Todos left</div> 
     </>
   );
 }
