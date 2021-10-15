ae3e6227071adbfbd56d6d259498f540157e2dd7
TravisSLee <travisslee223@gmail.com>
2021-10-15 17:13:53 -0400

    finish app

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,51 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import TodoList from './TodoList'
-import uuidv4 from './uuidv4'
+import {uuid} from 'uuidv4'
+
 
 function App() {
   
   const [todos, setTodos ] = useState([])
-  const todoNameRef = useRef
+  
+  const todoNameRef = useRef()
+  
+  useEffect(() => {
+    const storedTodos = JSON.parse(window.localStorage.getItem('todos'))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => { 
+    window.localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+  function toggledTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.completed = !todo.completed
+    setTodos(newTodos)
+  }
 
   function handleAddTodo(e){
     const name = todoNameRef.current.value
     if (name === '') return
     setTodos(prevTodos => {
-      return [...prevTodos, ]
+      return [...prevTodos, { id: uuid(), name: name, 
+        completed: false}]
     })
     todoNameRef.current.value = null
   }
   
+  function handleClearTodos(){
+    const newTodos = todos.filter(todo => !todo.completed)
+    setTodos(newTodos)
+  }
   return (
     <>
-      <TodoList todos={todos}/>
+      <TodoList todos={todos} toggledTodo={toggledTodo} />
       <input ref={todoNameRef} type="text" />
       <button type="button" onClick={handleAddTodo}>Add Todo</button>
-      <button type="button">Clear Todos</button>
-      <div>{todos.length} Todos left</div> 
+      <button type="button" onClick={handleClearTodos}>Clear Todos</button>
+      <div>{todos.filter(todo => !todo.completed).length} Todos left</div> 
     </>
   );
 }
